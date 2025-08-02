8c87af89ae82f71388da4f0294782c43524f3584
Rahul Rawat <rsr2002rawat@gmail.com>
2025-08-02 21:23:18 +0530

    arrays addr formula

---
+++ Batch4_DSA_2025/ARRAYS/c1.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main(){
+
+    int arr[5]={10,20,30,40,50};
+
+    double arr1[5]={7.3, 9.3, 0.2 ,1.1, 2.1};
+    
+    // for(int i=0; i<5; i++){
+    //     printf("%d \n",&arr[i]);
+    // }
+
+    printf("%d\n",&arr1[0]);
+    printf("%d\n",&arr1[1]);
+    
+    // int a=5;
+    // int b=10;
+    // printf("%d ",&a);
+    // printf("%d ",&b);
+
+    Addr= BA + (no of elements before the given element)*(size of a single element )
+    
+        = 1000+(4)*4
+        = 1016
+    
+    return 0;
+}
