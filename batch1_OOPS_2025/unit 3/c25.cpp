a64ab4f4bef90f3fccb5b950ff9f73185566fe2f
Rahul Rawat <rsr2002rawat@gmail.com>
2025-08-06 21:05:21 +0530

    assignment Ques 6

--- batch1_OOPS_2025/unit 3/c25.cpp
+++ batch1_OOPS_2025/unit 3/c25.cpp
@@ -1,8 +1,95 @@
 #include <iostream>
 using namespace std;
+class Student
+{
+private:
+    int marks;
+    int rollNo;
+    string name;
+
+public:
+    void setDetails()
+    {
+        cout << "SET Roll Number:  ";
+        cin >> rollNo;
+        cout << "SET Name: ";
+        cin >> name;
+        cout << "Enter Marks";
+        cin >> marks;
+    }
+
+    void set_marks(int val)
+    {
+        if (val >= 0 && val <= 100)
+        {
+            marks = val;
+        }
+        else
+        {
+            cout << "invalid number" << endl;
+        }
+    }
+
+    int get_marks()
+    {
+        return marks;
+    }
+
+    void display()
+    {
+        cout << "-----------------------------------------" << endl;
+        cout << "Roll number: " << rollNo << endl;
+        cout << "Name: " << name << endl;
+        cout << "Marks: " << marks << endl;
+        cout << "-----------------------------------------" << endl;
+    }
+    Student()
+    {
+        rollNo = 0;
+        name = "Unnamed";
+        marks = 0;
+    }
+    Student(int rollNo, string name, int m)
+    {
+        this->rollNo = rollNo;
+        this->name = name;
+        this->marks = m;
+    }
+
+    Student(int rollNo)
+    {
+        this->rollNo = rollNo;
+        name = "Default";
+        marks = 0;
+    }
+    friend void myPrinter(Student o1);
+    friend void myPrinter2(Student o1);
+};
+
+void myPrinter(Student o1)
+{
+    cout << "my name is " << o1.name << "." << endl;
+    cout << "my roll number is " << o1.rollNo << "." << endl;
+    cout << "I got " << o1.marks << " marks in my test." << endl;
+    // cout<<s1.marks;
+}
+
+void myPrinter2(Student o1)
+{
+    cout << o1.name;
+}
 
 int main()
 {
-
-    return 0;
+    Student s1;
+    // cout<<s1.marks;
+    s1.setDetails();
+    myPrinter(s1);
+    // s1.display();
+    // Student s2(101, "Shaurya", 87);
+    // s2.display();
+    // Student s3(102);
+    // s3.set_marks(100);
+    // s3.set_marks(37);
+    // s3.display();
 }
---
+++ ip2025/codes/loops1.py
@@ -0,0 +1,83 @@
+# [1,2,3,4,5,6,7,8,9,10]
+# print("Hello World!!!")
+
+# table 
+# num=int(input("enter a num: "))
+# for i in range(1,11):
+#     print(num*i)
+
+
+#print first 10 even numbers
+# for i in range(1,11):
+#     print(2*i)
+
+#print first 10 even numbers without using arithmetic operators
+# for i in range(2,21,2):
+#     print(i)
+
+#print first n even numbers
+# n=int(input("enter a number: "))
+# for i in range(1,n+1):
+#     print(2*i)
+
+#print first n even numbers without athematic operator
+# n=int(input("enter a number: "))
+# for i in range(2,2*n+1,2):
+#     print(i)
+
+# fruits=["apples","banana","guava","cherry","mango"]
+# print(fruits[0])
+# print(fruits[1])
+# print(fruits[2])
+# print(fruits[3])
+# print(fruits[4])
+# for i in range(0,5):
+#     print(fruits[i])
+# for value in fruits:
+#     print(value)
+
+#print all the negative values from a list
+# numbers=[5,88,-2,-7,11,-9,100,-1000,9,-44]
+# for value in numbers:
+#     if value<0:
+#         print(value)
+
+# *****
+# *****
+# *****
+# *****
+# *****
+
+# print("*****")
+# print("*****")
+# print("*****")
+# print("*****")
+# print("*****")
+# star="*"
+# for i in range(1,6):
+#     # print("* * * * * ")
+#     for i in range(1,6):
+#         print(star,end=" ")
+#     print("")
+
+# *****
+
+# for i in range(1,6):
+#     print(star,end=" ")
+# print("")
+# for i in range(1,6):
+#     print(star,end=" ")
+# print("")
+# for i in range(1,6):
+#     print(star,end=" ")
+# print("")
+# for i in range(1,6):
+#     print(star,end=" ")
+# print("")
+# for i in range(1,6):
+#     print(star,end=" ")
+
+# for j in range(1,4): #
+#     for i in range(1,6): #
+#         print(i)
+
